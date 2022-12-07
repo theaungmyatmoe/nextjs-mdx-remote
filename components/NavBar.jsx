98398878c9a44b84f6a672cf59bd470aae75646c
amm834 <aungmyatmoe834@gmail.com>
2022-12-08 00:00:03 +0630

    fix: navabar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,25 +3,17 @@ import Link from "next/link";
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container">
-                <Link className="navbar-brand" href="/">Blog</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link href="/" className="nav-link">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/bio">Bio</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <ul className="nav">
+            <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" href="/bio">Bio</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link disabled" href="/blog">Blog</Link>
+            </li>
+        </ul>
     )
 };
 
